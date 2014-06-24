eb7a5e3f83005db6e711102d69219c97ff4760d8
cdehuang <caroline.d.huang@gmail.com>
2014-06-24 13:14:16 -0400

    Update edit.py
    Made it a bit easier to use.

--- edit.py
+++ edit.py
@@ -4,9 +4,10 @@ import re
 import fileinput
 
 filenames = []
+sn_type = 'z40G'
 #originally this walked between the directories by itself, but it would fail somewhere and was taking a long time to fix, so I just got lazy and changed the directory names.
 #put a spectra.out.6.1 file in the research/Sn project directory to play with don't know why it's not working, just decided to not make it walk from lanl.ccn.spectra cause it's going wrong somewhere
-for root, dirs, files in os.walk('Lanl.ccsn.spectra/z40G'):
+for root, dirs, files in os.walk('Lanl.ccsn.spectra/{}'.format(sn_type)):
     #gets the path of the files
     for name in files:
         filenames.append(os.path.join(root, name))
