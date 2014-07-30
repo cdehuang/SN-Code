ee0ca6668ed0fdc46073921322c872237e6b7e17
cdehuang <caroline.d.huang@gmail.com>
2014-07-30 13:51:12 -0400

    Create rdmap.py
    Makes binary value fits fites that show the desired regions. Need to run this to make the weight maps before the other stuff can be done. Not all of the clusters are on here yet since a couple of them are missing the necessary acs mosaics.

---
+++ sims/rdmap.py
@@ -0,0 +1,28 @@
+import pyfits
+import numpy as np
+import os.path
+
+
+#here are all the clusters that have acs files, so I can make rdmaps for all of these, not all of them have deflection x and deflection y files though
+clusters = ['abell1423', 'abell209', 'abell2261', 'abell611', 'macs0329', 'macs0416','macs0429', 'macs0647', 'macs0717', 'macs0744', 'macs1115', 'macs1149', 'macs1206', 'macs1311', 'macs1423', 'macs1720', 'macs1931', 'macs2129', 'rxj1347', 'rxj1532', 'rxj2129', 'rxj2248']
+
+fitsfiles = ['a1423_mosaic_065mas_acs_wfc3ir_total_drz_20130227.fits.gz', 'a209_mosaic_065mas_acs_wfc3ir_total_drz_20130119.fits.gz', 'a2261_mosaic_065mas_acs_wfc3ir_total_drz_20130123.fits.gz', 'a611_mosaic_065mas_acs_wfc3ir_total_drz_20120618.fits.gz', 'macs0329_mosaic_065mas_acs_wfc3ir_total_drz_20111105.fits.gz', 'macs0416_mosaic_065mas_acs_wfc3ir_total_drz_20130119.fits.gz', 'macs0429_mosaic_065mas_acs_wfc3ir_total_drz_20130227.fits.gz', 'macs0647_mosaic_065mas_acs_wfc3ir_total_drz_20111202.fits.gz', 'macs0717_mosaic_065mas_acs_wfc3ir_total_drz_20111230.fits.gz', 'macs0744_mosaic_065mas_acs_wfc3ir_total_drz_20111230.fits.gz', 'macs1115_mosaic_065mas_acs_wfc3ir_total_drz_20120305.fits.gz', 'macs1149_mosaic_65mas_acs_wfc3ir_total_drz_20110825.fits.gz','macs1206_mosaic_065mas_acs_wfc3ir_total_drz_20110815.fits.gz','macs1311_mosaic_065mas_acs_wfc3ir_total_drz_20130709.fits', 'macs1423_mosaic_065mas_acs_wfc3ir_total_drz_20130313.fits.gz', 'macs1720_mosaic_065mas_acs_wfc3ir_total_drz_20120628.fits.gz', 'macs1931_mosaic_065mas_acs_wfc3ir_total_drz_20120628.fits.gz', 'macs2129_mosaic_065mas_acs_wfc3ir_total_drz_20120122.fits.gz', 'rxj1347_mosaic_065mas_acs_wfc3ir_total_drz_20110812.fits.gz', 'rxj1532_mosaic_065mas_acs_wfc3ir_total_drz_20120416.fits.gz', 'rxj2129_mosaic_065mas_acs_wfc3ir_total_drz_20121016.fits.gz', 'rxj2248_mosaic_065mas_acs_wfc3ir_total_drz_20130119.fits.gz']
+
+clusterdict = dict(zip(clusters, fitsfiles))
+
+for i,j in clusterdict.items():
+    if os.path.isfile('{}/weight.fits'.format(i)) is False:
+        hdulist = pyfits.open('{}/{}'.format(i,j))
+        imdata = hdulist[0].data
+
+        nonzero = np.nonzero(imdata)
+        imdata[nonzero[0], nonzero[1]] = 1
+        hdulist.writeto('{}/weight.fits'.format(i))
+
+#to get the shape
+#imdata.shape
+#imdata.dtype.shape
+
+#change all of the nonzero pixel values to 1
+
+#can't save changes back to original fits file unless you unzip it, so just do this. But you can just go through and make the weights for everything. 
